d90c02ea519795d44f8f0f5326dd5c84edf8c128
Caterina Bertotti <bertotti4997@gmail.com>
2021-03-22 17:20:17 +0100

    Snack 3 pomeriggio

--- snack 3 pomeriggio/js/script.js
+++ snack 3 pomeriggio/js/script.js
@@ -12,22 +12,6 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-/** 
-* arrayInterval - verifica i valori compresi tra due posizioni (comprese)
-* 
-* @param  {number} a //numero più piccolo 
-* @param  {number} b //numero più grande
-* @param  {array} array //narray di riferimento
-* @return {array} //un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
-// */
-function arrayInterval(a, b, array) {
-    var arryInt = [];
-    for ( var i = a; i <= b && < array.length; i++ ) {
-        arryInt.push(array[i]);
-    }
-    return arryInt;
-} 
-
 const numbers = [];
 
 for ( var y = 0; y < 20; y++ ) {
@@ -36,5 +20,16 @@ for ( var y = 0; y < 20; y++ ) {
 }
 console.log(numbers);
 
-const Interval = arrayInterval(3, 10, numbers);
-console.log(Interval);
+do {
+    var numeroA = parseInt(prompt('Inserisci un numero A da 0 a 20'));
+} while ( numeroA < 0 || numeroA > 20);
+
+do {
+    var numeroB = parseInt(prompt('Inserisci un numero B da 0 a 20'));
+} while ( numeroB < numeroA || numeroB > 20 );
+
+
+const newArray = numbers.filter((item) => {
+    return numbers.indexOf(item) >= numeroA && numbers.indexOf(item) <= numeroB;
+});
+console.log(newArray);
